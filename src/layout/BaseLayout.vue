c472d6d829ee2865f75ec42e8bab18cf187e9d67
llebedev <llebedev@dallilogisticgroup.com>
2023-05-19 15:07:44 +0300

    Refactor product view with BaseLayout component.

---
+++ src/layout/BaseLayout.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="h-screen">
+    <!-- Header -->
+    <div class="mx-6 mt-6">
+      <slot name="header">
+        <h1 class="text-center text-3xl">Product Card</h1>
+      </slot>
+    </div>
+
+    <!-- Main -->
+    <main class="m-6">
+      <slot />
+    </main>
+
+    <!-- Footer -->
+    <footer class="pb-6">
+      <slot name="footer">
+        <p class="text-center text-xl">Product Card Footer</p>
+      </slot>
+    </footer>
+  </div>
+</template>
--- src/views/ProductView.vue
+++ src/views/ProductView.vue
@@ -1,37 +1,38 @@
 <template>
-  <div>
-    <h1 class="text-center text-3xl my-3">Product Card</h1>
+  <BaseLayout>
+    <div>
+      <div class="flex items-center justify-center my-6">
+        <input v-model="searchQuery" class="input-class mr-4" placeholder="Поиск по цене или названию" type="text" />
 
-    <div class="flex items-center justify-center my-6">
-      <input v-model="searchQuery" class="input-class mr-4" placeholder="Поиск по цене или названию" type="text" />
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
+          @click="isProductModal = true"
+        >
+          Добавить товар
+        </button>
+      </div>
 
-      <button
-        class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-3 rounded"
-        @click="isProductModal = true"
+      <p v-if="isLoading" class="text-center text-xl mt-8">Идет загрузка...</p>
+
+      <p v-if="!filteredItems.length && !isLoading" class="text-center text-xl mt-8">Таких товаров нет...</p>
+
+      <div
+        v-else
+        class="sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 mx-auto my-6 gap-4 place-items-start"
       >
-        Добавить товар
-      </button>
+        <ProductCard v-for="item in filteredItems" :key="item.id" :product="item" />
+      </div>
+
+      <AddProductModal v-if="isProductModal" @close-modal="isProductModal = false" @add-product="addProduct" />
     </div>
-
-    <p v-if="isLoading" class="text-center text-xl mt-8">Идет загрузка...</p>
-
-    <p v-if="!filteredItems.length && !isLoading" class="text-center text-xl mt-8">Таких товаров нет...</p>
-
-    <div
-      v-else
-      class="sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 grid grid-cols-1 mx-auto my-6 gap-4 place-items-start"
-    >
-      <ProductCard v-for="item in filteredItems" :key="item.id" :product="item" />
-    </div>
-
-    <AddProductModal v-if="isProductModal" @close-modal="isProductModal = false" @add-product="addProduct" />
-  </div>
+  </BaseLayout>
 </template>
 
 <script setup>
   import { computed, defineAsyncComponent, onMounted, ref } from 'vue';
   import ProductCard from '@/components/ProductCard.vue';
   import { useDataCard } from '@/hooks/useDataCard.js';
+  import BaseLayout from '@/layout/BaseLayout.vue';
 
   const AddProductModal = defineAsyncComponent(() => import('@/components/AddProductModal.vue'));
 
